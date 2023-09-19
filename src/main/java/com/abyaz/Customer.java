0bea70f10c3f43694686ee51808ccca9480323fd
Muhammad Abyaz <MuhammadAbyaz@users.noreply.github.com>
2023-09-19 17:53:25 +0500

    project completed

--- src/main/java/com/abyaz/Customer.java
+++ src/main/java/com/abyaz/Customer.java
@@ -10,54 +10,59 @@ public class Customer {
     @Id
     @SequenceGenerator(
             name = "customer-id-sequence",
-            sequenceName = "customer-id-sequence"
+            sequenceName = "customer-id-sequence",
+            allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
             generator = "customer-id-sequence"
     )
-    private Integer _id;
-    private String _name;
-    private String _email;
-    private Integer _age;
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
 
 
     public Customer(Integer id,String name, String email, Integer age){
-        this._name = name;
-        this._email = email;
-        this._age = age;
-        this._id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.id = id;
     }
 
     public Customer() {
     }
 
-    public String get_name() {
-        return _name;
+    public Integer getId() {
+        return id;
     }
 
-    public void set_name(String _name) {
-        this._name = _name;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public String get_email() {
-        return _email;
+    public String getName() {
+        return name;
     }
 
-    public void set_email(String _email) {
-        this._email = _email;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int get_age() {
-        return _age;
+    public String getEmail() {
+        return email;
     }
 
-    public void set_age(int _age) {
-        this._age = _age;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public int get_id() {
-        return _id;
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     @Override
@@ -65,21 +70,21 @@ public class Customer {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Customer customer = (Customer) o;
-        return _age == customer._age && _id == customer._id && Objects.equals(_name, customer._name) && Objects.equals(_email, customer._email);
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(_name, _email, _age, _id);
+        return Objects.hash(id, name, email, age);
     }
 
     @Override
     public String toString() {
         return "Customer{" +
-                "_name='" + _name + '\'' +
-                ", _email='" + _email + '\'' +
-                ", _age=" + _age +
-                ", _id=" + _id +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
                 '}';
     }
 }
